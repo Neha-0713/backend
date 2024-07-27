9206c724715cf1c3e42b04fb72df9eb97f191f3f
Neha-0713 <nrai52703@gmail.com>
2024-07-27 16:13:26 +0530

    database operations

--- routers/ProductRouter.js
+++ routers/ProductRouter.js
@@ -14,4 +14,20 @@ router.post('/add', (req, res)=>{
     });
 })
 
+router.get('/getall',(req, res)=>{
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result)
+    }).catch((err) => {
+       console.log(err);
+       res.status(500).json(err) 
+    });
+
+    router.get('/getbyid/:id',(req,res)=>{
+        Model.find(req.params.id)
+        
+    })
+
+})
+
 module.exports=router;
