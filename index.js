09cdb97e4026087ce281bd556d8d37a209a74323
Neha-0713 <nrai52703@gmail.com>
2024-08-01 02:45:46 +0530

    product model

--- index.js
+++ index.js
@@ -1,48 +1,41 @@
-//importing express
-const express = require('express');
-const UserRouter=require('./routers/UserRouter');
-const ProductRouter=require('./routers/ProductRouter');
+// importing express
+const express = require("express");
+const UserRouter = require("./routers/UserRouter");
+const ProductRouter = require("./routers/ProductRouter")
 const cors = require('cors');
-
-//initialize express
+// initialize express
 const app = express();
-
 const port = 5000;
 
-//middleware
-app.use(cors({
-    origin : 'http://localhost:3000'
-}));
-app.use(express.json());
-app.use('/user', UserRouter);
-app.use('/products', ProductRouter);
+// these are middlewares
+app.use(cors({origin:'http://localhost:3000'}))
+app.use(express.json()); //sequence is imp it firsts convert all json format to javascript to be interpreted by js file here
+app.use("/product", ProductRouter);
+app.use("/user", UserRouter);
 
-//end point or route(to let the server accept the request)
-app.get('/',(req, res)=>{
-    res.send('response from express');
+
+
+///endpoint or route
+app.get("/", (req, res) => {
+  res.send("Response from express");
+});
+app.get("/add", (req, res) => {
+  res.send("Response from Add");
+});
+app.get("/getall", (req, res) => {
+  res.send("Response from get All");
+});
+app.get("/update", (req, res) => {
+  res.send("Response from get Update");
 });
 
-app.get('/add',(req, res)=>{
-    res.send('response from add')
-});
+//starting the server
+app.listen(port, (err) => {
+  if(err){
+    console.log("failed" + err);
+  }
+  else { 
+    console.log("Server started successfully");
 
-app.get('/all',(req, res)=>{
-    res.send('ressponse from all')
-});
-
-app.get('/update',(req,res)=>{
-    res.send('update')
-});
-
-
-//starting the  express server
-app.listen(port, (err)=>{
-    if(err){
-        console.log("failed"+err);
-    }
-    else{
-        console.log('server started');
-    }
-    
-
+  }
 });
