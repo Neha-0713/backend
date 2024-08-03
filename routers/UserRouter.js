6c337ca3ac7b01cbb11ec4a1cfc397d46a29a1b7
Neha-0713 <nrai52703@gmail.com>
2024-08-03 17:29:12 +0530

    token verification

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,130 +1,110 @@
 const express = require('express');
-
-const router=express.Router();
 const Model = require('../models/UserModel');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-router.post('/add', (req, res)=>{
+const router = express.Router();
+
+router.post('/add', (req, res) => {
     console.log(req.body);
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);//500 is server side error response status code
-    });
-
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.get('/getall', (req, res) => {
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+// : denotes url parameter
+router.get('/getbycity/:city', (req, res) => {
+    console.log(req.params.city);
+    Model.find({ city: req.params.city })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.get('/getbyemail/:email', (req, res) => {
+    Model.findOne({ email: req.params.email })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/getall', (req, res)=>{
-    Model.find()
-    .then((result) => {
-        res.status(200).json(result); 
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
-
-//colon denotes url parameter
-router.get('/getbycity/:city' , (req, res)=>{
-    console.log(req.params.city);
-
-    Model.find({city:req.params.city})
-    .then((result) => {
-        res.status(200).json(result); 
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-
-})
-
-router.get('/getbyemail/:email',(req, res)=>{
-    Model.findOne({email:req.params.email})
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
-
-router.get('/getbyid/:id', (req, res)=>{
-    Model.findById(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
-
-router.delete('/delete/:id', (req, res)=>{
-    Model.findByIdAndDelete(req.params.id)
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
-
-router.put('/update/:id',(req,res)=>{
-    Model.findByIdAndUpdate(req.params.id,req.body,{new:true})
-    .then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-})
-
-router.post('/authenticate', (req,res)=>{
+router.post('/authenticate', (req, res) => {
     Model.findOne(req.body)
-    .then((result) => {
-        if(result){
-            //payload, secretkry, expiry- requirements for token generation
-            const {_id, email, password} = result;
-            const payload = {_id, email, password};
+        .then((result) => {
+            if (result) {
 
-            jwt.sign(
-                payload,
-                process.env.JWT_SECRET,
-                {expiresIn: '1hr'},
-                (err, token)=>{
-                    if(err){
-                        console.log(err);
-                        res.status(500).json(err);
-                    }else{
-                        res.status(200).json({token: token});
+                // payload, secretkey, expiry
+
+                const { _id, email, password } = result;
+                const payload = { _id, email, password };
+
+                jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    { expiresIn: '1hr' },
+                    (err, token) => {
+                        if(err) {
+                            console.log(err);
+                            res.status(500).json(err);
+                        }else{
+                            res.status(200).json({ token: token });
+                        }
                     }
-                    
-                }
-            )
+                )
 
-
-        }else{
-            res.status(401).json({message:'Invalid Credentials'})
-        }
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
 
-
-module.exports=router;
-
-//grtall
-//getbyid
-//update
-//delete
-
-//npm init -y
-//npm i express
-//npm install nodemon
-//add dev script
+module.exports = router;
